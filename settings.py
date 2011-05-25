bb05ca3af07c98fd1a60aa7ba3c4dcf2c55b3c75
Tomas Edwardsson <tommi@tommi.org>
2011-05-25 23:17:51 +0000

    Added external css/js support to templates

--- settings.py
+++ settings.py
@@ -49,12 +49,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = "%s/media" % (djangopath)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
