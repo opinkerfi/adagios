4d52656126bfabf3db15c50a4e9d2d98c3ab8b66
Pall Sigurdsson <palli@opensource.is>
2011-05-30 20:20:30 +0000

    .pyc files removed. Bugfixes in rest module

--- rest/views.py
+++ rest/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render_to_response, redirect
 from django.core import serializers
 from django.http import HttpResponse, HttpResponseServerError
 from django.utils import simplejson
+#from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
 
 my_module = None
 
@@ -11,7 +13,7 @@ def _load(module_name):
     #if not my_module:
     my_module = __import__(module_name, fromlist=[''])
     return my_module
-    
+@csrf_exempt   
 def handle_request(request, module_name, attribute, format):
     m = _load(module_name)
     # TODO: Only allow function calls if method == POST
@@ -26,10 +28,11 @@ def handle_request(request, module_name, attribute, format):
             arguments = request.GET
             result = item( **arguments )
     elif request.method == 'POST':
-        item = okconfig.__dict__[attribute]
+        #c.update(csrf(request))
+        item = m.__dict__[attribute]
         item_type = str(type(item))
         if item_type != "<type 'function'>":
-            result = okconfig.__dict__[attribute]
+            result = m.__dict__[attribute]
         else:
             arguments = request.POST
             result = item( **arguments )
@@ -38,14 +41,18 @@ def handle_request(request, module_name, attribute, format):
     if format == 'json':
         import json
         result = json.dumps( result )
+        mimetype='application/javascript'
     elif format == 'xml':
             import xml.marshal.generic
             result = xml.marshal.generic.dumps(result)
+            mimetype='application/xml'
     elif format == 'txt':
         result = str(result)
+        mimetype='text/plain'
     else:
         result = str(result)
-    return HttpResponse(result, mimetype='application/javascript')
+        mimetype='text/plain'        
+    return HttpResponse(result, mimetype=mimetype)
 
 def index( request, module_name ):
     m = _load(module_name)
