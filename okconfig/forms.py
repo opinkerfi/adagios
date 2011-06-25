374d87380d2806bdb6c77038b632f5743c1fc5fa
Pall Sigurdsson <palli@opensource.is>
2011-06-25 01:10:52 +0000

    scannednetwork ip validation fixed

--- okconfig/forms.py
+++ okconfig/forms.py
@@ -24,7 +24,9 @@ class ScanNetworkForm(forms.Form):
         if hostname[-1:] == ".":
             hostname = hostname[:-1] # strip exactly one dot from the right, if present
         allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
-        return all(allowed.match(x) for x in hostname.split("."))
+        for x in hostname.split("."):
+            if allowed.match(x) is False: return False
+        return True
 
 class AddGroupForm(forms.Form):
     group_name = forms.CharField()
