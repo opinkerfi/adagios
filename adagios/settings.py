c36a02cc99046aee87cbbe1971afd63a3d584291
Tomas Edwardsson <tommi@tommi.org>
2012-05-09 13:29:02 +0000

    Reset djangopath to discovery

--- adagios/settings.py
+++ adagios/settings.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 # Hack to allow relative template paths
 import os
-djangopath = '/home/tommi/coding/adagios/adagios'
+djangopath = os.path.dirname(__file__)
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
