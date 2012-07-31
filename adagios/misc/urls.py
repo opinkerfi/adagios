aa8575fcbc433553ab7047373a78d6c70714f934
Tomas Edwardsson <tommi@tommi.org>
2012-07-31 09:15:52 +0000

    Removed /misc/ from urls.py
    When using "url misc.views.index" Django was defaulting to showing /misc
    instead of expected / for Home link and clicking the logo.

--- adagios/misc/urls.py
+++ adagios/misc/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    (r'^/?$', 'misc.views.index'),
     (r'^/contact_us/?', 'misc.views.contact_us'),
     (r'^/test/?', 'misc.tests.test'),
     (r'^/settings/?', 'misc.views.settings'),
