ebdbd5e3163f7f2fda5a44f6f22a15a4936f8d07
Pall Sigurdsson <palli@opensource.is>
2012-08-02 16:21:37 +0000

    django.core.context_processors.tz removed

--- adagios/settings.py
+++ adagios/settings.py
@@ -112,7 +112,6 @@ TEMPLATE_CONTEXT_PROCESSORS = ('adagios.context_processors.resolve_urlname',
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
-    "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages")
 
  
