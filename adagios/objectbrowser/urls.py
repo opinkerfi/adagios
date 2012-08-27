a405aa886ab6382b30a6fcc63f36278b9e0c5038
Pall Sigurdsson <palli@opensource.is>
2012-08-27 20:20:28 +0000

    copy object for a single object perfected

--- adagios/objectbrowser/urls.py
+++ adagios/objectbrowser/urls.py
@@ -13,8 +13,9 @@ urlpatterns = patterns('',
     url(r'^/$', 'objectbrowser.views.list_object_types', name="objectbrowser"),
     (r'^/search/?$', 'objectbrowser.views.list_objects'),
     (r'^/id=(?P<object_id>.+)$', 'objectbrowser.views.edit_object'),
-    url(r'/edit_object/id=(?P<object_id>.+)$', 'objectbrowser.views.edit_object', name="edit_object"),
-    (r'/delete_object/id=(?P<object_id>.+)$', 'objectbrowser.views.delete_object'),
+    url(r'/edit/id=(?P<object_id>.+)$', 'objectbrowser.views.edit_object', name="edit_object"),
+    url(r'/copy/id=(?P<object_id>.+)$', 'objectbrowser.views.copy_object', name="copy_object"),
+    url(r'/delete/id=(?P<object_id>.+)$', 'objectbrowser.views.delete_object', name="delete_object"),
     url(r'/bulk_edit/?$', 'objectbrowser.views.bulk_edit', name='bulk_edit'),
     url(r'/bulk_delete/?$', 'objectbrowser.views.bulk_delete', name='bulk_delete'),
     url(r'/bulk_copy/?$', 'objectbrowser.views.bulk_copy', name='bulk_copy'),
@@ -27,7 +28,14 @@ urlpatterns = patterns('',
     (r'/advanced_edit/id=(?P<object_id>.+)$', 'objectbrowser.views.advanced_edit'),
     #(r'^$', 'objectbrowser.views.list_object', { 'object_type':"None" } ),
     #(r'^/(contact|host|service|timeperiod|command|contactgroup|hostgroup|servicegroup)', 'objectbrowser.views.list_object'),
-    
+
+    # These should be deprecated as of 2012-08-27
+    (r'^/search/?$', 'objectbrowser.views.list_objects'),
+    (r'^/id=(?P<object_id>.+)$', 'objectbrowser.views.edit_object'),
+    (r'/edit_object/id=(?P<object_id>.+)$', 'objectbrowser.views.edit_object'),
+    (r'/copy_object/id=(?P<object_id>.+)$', 'objectbrowser.views.copy_object'),
+    (r'/delete_object/id=(?P<object_id>.+)$', 'objectbrowser.views.delete_object'),
+
     # 
     #(r'/(?P<object_type>.+)/?$', 'objectbrowser.views.list_objects'),
     
