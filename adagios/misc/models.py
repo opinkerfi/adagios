6a91e6dda7136d669f63b9a5e4470da876ce775a
Pall Sigurdsson <palli@opensource.is>
2013-06-12 15:24:56 +0000

    model class for BPI

--- adagios/misc/models.py
+++ adagios/misc/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class TestModel(models.Model):
+    testField = models.CharField(max_length=100)
+    testField2 = models.CharField(max_length=100)
+
+class BusinessProcess(models.Model):
+    processes = models.ManyToManyField("self", unique=False, blank=True)
+    name = models.CharField(max_length=100, unique=True)
+    display_name = models.CharField(max_length=100, blank=True)
+    notes = models.CharField(max_length=1000, blank=True)
+    #graphs = models.ManyToManyField(BusinessProcess, unique=False, blank=True)
+    #graphs = models.ManyToManyField(BusinessProcess, unique=False, blank=True)
+
+class Graph(models.Model):
+    host_name = models.CharField(max_length=100)
+    service_description = models.CharField(max_length=100)
+    metric_name = models.CharField(max_length=100)
+
+
