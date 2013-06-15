7245440d9da7c7978c9d873328bba738d468b413
Pall Sigurdsson <palli@opensource.is>
2013-06-15 02:41:25 +0000

    * Improve unicode handling in objectbrowser forms

--- adagios/forms.py
+++ adagios/forms.py
@@ -6,8 +6,12 @@ class AdagiosForm(forms.Form):
     """ Base class for all forms in this module. Forms that use pynag in any way should inherit from this one.
     """
     def clean(self):
-        cleaned_data = super(AdagiosForm, self).clean()
-        for k,v in cleaned_data.items():
-            if isinstance(v, (str,unicode)):
-                cleaned_data[k] = smart_str(v)
+        cleaned_data = {}
+        tmp = super(AdagiosForm, self).clean()
+        for k,v in tmp.items():
+            if isinstance(k, (unicode)):
+                k = smart_str(k)
+            if isinstance(v, (unicode)):
+                v = smart_str(v)
+            cleaned_data[k] = smart_str(v)
         return cleaned_data
