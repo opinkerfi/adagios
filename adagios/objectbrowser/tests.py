fbd2e631e5c400c5a849d5aceded58b72818cc87
Pall Sigurdsson <palli@opensource.is>
2014-02-25 18:14:12 +0000

    Added smoketests for objectbrowser

--- adagios/objectbrowser/tests.py
+++ adagios/objectbrowser/tests.py
@@ -20,3 +20,36 @@ class TestObjectBrowser(unittest.TestCase):
         c = Client()
         response = c.get('/objectbrowser/')
         self.assertEqual(response.status_code, 200)
+
+    def testPageLoad(self):
+        """ Smoke test a bunch of views """
+
+        # TODO: Better tests, at least squeeze out a 200OK for these views
+        self.loadPage('/objectbrowser/')
+        self.loadPage('/objectbrowser/new_edit', 301)
+        self.loadPage('/objectbrowser/copy', 404)
+        self.loadPage('/objectbrowser/add', 404)
+        self.loadPage('/objectbrowser/search')
+        self.loadPage('/objectbrowser/delete', 404)
+        self.loadPage('/objectbrowser/bulk_edit')
+        self.loadPage('/objectbrowser/bulk_delete')
+        self.loadPage('/objectbrowser/bulk_copy')
+
+        self.loadPage('/objectbrowser/edit_all', 404)
+        self.loadPage('/objectbrowser/copy_and_edit', 301)
+
+        self.loadPage('/objectbrowser/confighealth')
+        self.loadPage('/objectbrowser/plugins')
+        self.loadPage('/objectbrowser/nagios.cfg')
+        self.loadPage('/objectbrowser/geek_edit', 404)
+        self.loadPage('/objectbrowser/advanced_edit', 404)
+
+        #self.loadPage('/objectbrowser/add_to_group')
+    def loadPage(self, url, expected_code=200):
+        """ Load one specific page, and assert if return code is not 200 """
+        try:
+            c = Client()
+            response = c.get(url)
+            self.assertEqual(response.status_code, expected_code, "Expected status code 200 for page %s" % url)
+        except Exception, e:
+            self.assertEqual(True, "Unhandled exception while loading %s: %s" % (url, e))
