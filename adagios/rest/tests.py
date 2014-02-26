1b79cee8797e206d410a1d832ec332a36c4f068f
Pall Sigurdsson <palli@opensource.is>
2014-02-26 17:35:07 +0000

    /rest/ - added more unit tests

--- adagios/rest/tests.py
+++ adagios/rest/tests.py
@@ -5,20 +5,44 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.utils import unittest
+from django.test.client import Client
+import json
 
 
-class SimpleTest(TestCase):
+class LiveStatusTestCase(unittest.TestCase):
+    def testPageLoad(self):
+        """ Smoke Test for various rest modules """
+        self.loadPage('/rest')
+        self.loadPage('/rest/status/')
+        self.loadPage('/rest/okconfig/')
+        self.loadPage('/rest/pynag/')
+        self.loadPage('/rest/adagios/')
+        self.loadPage('/rest/status.js')
+        self.loadPage('/rest/okconfig.js')
+        self.loadPage('/rest/pynag.js')
+        self.loadPage('/rest/adagios.js')
 
-    def test_basic_addition(self):
+    def testDnsLookup(self):
+        """ Test the DNS lookup rest call
         """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+        path = "/rest/pynag/json/dnslookup"
+        data = {'host_name': 'localhost'}
+        try:
+            c = Client()
+            response = c.post(path=path, data=data)
+            json_data = json.loads(response.content)
+            self.assertEqual(response.status_code, 200, "Expected status code 200 for page %s" % path)
+            self.assertEqual(True, 'addresslist' in json_data, "Expected 'addresslist' to appear in response")
+        except KeyError, e:
+            self.assertEqual(True, "Unhandled exception while loading %s: %s" % (path, e))
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+    def loadPage(self, url):
+        """ Load one specific page, and assert if return code is not 200 """
+        try:
+            c = Client()
+            response = c.get(url)
+            self.assertEqual(response.status_code, 200, "Expected status code 200 for page %s" % url)
+        except Exception, e:
+            self.assertEqual(True, "Unhandled exception while loading %s: %s" % (url, e))
