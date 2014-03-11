74f6e1feacc7a145401bdb90cbe19de641fded4c
Pall Sigurdsson <palli@opensource.is>
2014-03-11 13:43:56 +0000

    Fail softly if /etc/adagios/ssi does not exist

--- adagios/context_processors.py
+++ adagios/context_processors.py
@@ -74,28 +74,30 @@ def get_current_time(request):
 def get_serverside_includes(request):
     """ Returns a list of serverside includes to include on this page """
     result = {}
-    result['ssi_headers'] = []
-    result['ssi_footers'] = []
-    dirname = adagios.settings.serverside_includes
-    files = os.listdir(dirname)
-    current_url = resolve_urlname(request)
-    if not dirname:
+    try:
+        result['ssi_headers'] = []
+        result['ssi_footers'] = []
+        dirname = adagios.settings.serverside_includes
+        current_url = resolve_urlname(request)
+        if not dirname:
+            return {}
+        if not os.path.isdir(dirname):
+            return {}
+        files = os.listdir(dirname)
+        common_header_file = "common-header.ssi"
+        common_footer_file = "common-footer.ssi"
+        custom_header_file = "{urlname}-header.ssi".format(urlname=current_url)
+        custom_footer_file = "{urlname}-footer.ssi".format(urlname=current_url)
+        if common_header_file in files:
+            result['ssi_headers'].append(dirname + "/" + common_header_file)
+        if common_footer_file in files:
+            result['ssi_footers'].append(dirname + "/" + common_footer_file)
+        if custom_header_file in files:
+            result['ssi_headers'].append(dirname + "/" + custom_header_file)
+        if custom_footer_file in files:
+            result['ssi_footers'].append(dirname + "/" + custom_footer_file)
+    except Exception:
         return {}
-    if not os.path.isdir(dirname):
-        return {}
-    files = os.listdir(dirname)
-    common_header_file = "common-header.ssi"
-    common_footer_file = "common-footer.ssi"
-    custom_header_file = "{urlname}-header.ssi".format(urlname=current_url)
-    custom_footer_file = "{urlname}-footer.ssi".format(urlname=current_url)
-    if common_header_file in files:
-        result['ssi_headers'].append(dirname + "/" + common_header_file)
-    if common_footer_file in files:
-        result['ssi_footers'].append(dirname + "/" + common_footer_file)
-    if custom_header_file in files:
-        result['ssi_headers'].append(dirname + "/" + custom_header_file)
-    if custom_footer_file in files:
-        result['ssi_footers'].append(dirname + "/" + custom_footer_file)
     return result
 
 
