b3fbf05fcfdc20e57503efeb534dc5a8d6c20265
Philippe Pepos Petitclerc <ppepos@savoirfairelinux.com>
2014-03-11 16:22:11 -0400

    added option to group services in the same filename as their effective host

--- adagios/objectbrowser/__init__.py
+++ adagios/objectbrowser/__init__.py
@@ -15,6 +15,12 @@ def startup():
     # Pre load objects on startup
     pynag.Model.ObjectDefinition.objects.get_all()
 
+    # Associate services with their host in the written config files. 
+    # (If option is set in adagios.conf)
+    if settings.save_services_with_hosts:
+        for serv in pynag.Model.Service.objects.all:
+            serv.merge_service_to_host()
+
     if settings.enable_githandler == True:
         from pynag.Model import EventHandlers
         pynag.Model.eventhandlers.append(
