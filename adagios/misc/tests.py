3e951d0b47c3cda8708f6c67a5a6e4c0fe9c0545
Pall Sigurdsson <palli@opensource.is>
2014-04-10 11:22:37 +0000

    more tests added for misc module

--- adagios/misc/tests.py
+++ adagios/misc/tests.py
@@ -15,5 +15,22 @@ class MiscTestCase(unittest.TestCase):
         response = c.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def TestSettings(self):
-        self._testPageLoad('/misc/settings')
+    def TestPageLoads(self):
+        """ Smoke test views in /misc/
+        """
+        self.loadPage("/misc/settings")
+        self.loadPage("/misc/nagios")
+        self.loadPage("/misc/settings")
+        self.loadPage("/misc/service")
+        self.loadPage("/misc/pnp4nagios")
+        self.loadPage("/misc/mail")
+        self.loadPage("/misc/images")
+
+    def loadPage(self, url):
+        """ Load one specific page, and assert if return code is not 200 """
+        try:
+            c = Client()
+            response = c.get(url)
+            self.assertEqual(response.status_code, 200, _("Expected status code 200 for page %s") % url)
+        except Exception, e:
+            self.assertEqual(True, _("Unhandled exception while loading %(url)s: %(e)s") % {'url': url, 'e': e})
