0f382f69796c7fcedafd3239a1aa11a582aa1464
Pall Sigurdsson <palli@opensource.is>
2014-04-10 00:06:17 +0000

    Add missing smoketests for okconfig

--- adagios/okconfig_/tests.py
+++ adagios/okconfig_/tests.py
@@ -21,3 +21,24 @@ class TestOkconfig(unittest.TestCase):
         c = Client()
         response = c.get('/okconfig/verify_okconfig')
         self.assertEqual(response.status_code, 200)
+
+    def testPageLoad(self):
+        """ Smoketest for the okconfig views """
+        self.loadPage('/okconfig/addhost')
+        self.loadPage('/okconfig/scan_network')
+        self.loadPage('/okconfig/addgroup')
+        self.loadPage('/okconfig/addtemplate')
+        self.loadPage('/okconfig/addhost')
+        self.loadPage('/okconfig/addservice')
+        self.loadPage('/okconfig/install_agent')
+        self.loadPage('/okconfig/edit')
+        self.loadPage('/okconfig/edit/localhost')
+        self.loadPage('/okconfig/verify_okconfig')
+    def loadPage(self, url):
+        """ Load one specific page, and assert if return code is not 200 """
+        try:
+            c = Client()
+            response = c.get(url)
+            self.assertEqual(response.status_code, 200, _("Expected status code 200 for page %s") % url)
+        except Exception, e:
+            self.assertEqual(True, _("Unhandled exception while loading %(url)s: %(e)s") % {'url': url, 'e': e})
