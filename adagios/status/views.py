84b02b08eef6ad84ff0e796c45b0ba4d18e7c02a
petemorgan <petem@imaginesoftware.com>
2014-09-22 17:42:36 -0400

    Add support for enable_pnp4nagios flag

--- adagios/status/views.py
+++ adagios/status/views.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 #
 # Adagios is a web based Nagios configuration interface
@@ -326,13 +327,14 @@ def service_detail(request, host_name, service_description):
         c['errors'].append(e)
 
     # Lets get some graphs
-    try:
-        tmp = run_pnp("json", host=host_name)
-        tmp = json.loads(tmp)
-    except Exception, e:
-        tmp = []
-        c['pnp4nagios_error'] = e
-    c['graph_urls'] = tmp
+    if adagios.settings.enable_pnp4nagios:
+      try:
+          tmp = run_pnp("json", host=host_name)
+          tmp = json.loads(tmp)
+      except Exception, e:
+          tmp = []
+          c['pnp4nagios_error'] = e
+      c['graph_urls'] = tmp
     
     if adagios.settings.enable_graphite:
         metrics = [x.label for x in perfdata.metrics]
