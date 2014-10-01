dfd119db288dc168535ea4fa6a65c85594b15d49
Tomas Edwardsson <tommi@tommi.org>
2014-10-01 23:37:12 +0000

    Started work on ironing out support for django 1.6

--- adagios/objectbrowser/urls.py
+++ adagios/objectbrowser/urls.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import *
+from django.conf.urls import url, patterns
 
 
 urlpatterns = patterns('adagios',
