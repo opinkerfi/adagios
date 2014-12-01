663db55ab6f9c6652acc457c3f8c819f70fa98fa
Tomas Edwardsson <tommi@plainvanillagames.com>
2014-12-01 21:08:38 +0000

    Test subclassing pynag instead of mapping functions

--- adagios/daemon.py
+++ adagios/daemon.py
@@ -20,7 +20,21 @@
 from pynag.Control import daemon
 from adagios import settings
 
-class Daemon(object):
+class Daemon(daemon):
+    def __init__(self):
+        super(Daemon, self).__init__()
+
+        # Mapping needed
+        if settings.nagios_binary:
+            self.nagios_bin = settings.nagios_binary
+        if settings.nagios_config:
+            self.nagios_cfg = settings.nagios_config
+        if settings.nagios_init_script:
+            self.nagios_init = settings.nagios_init_script
+        if settings.nagios_service:
+            self.service_name = settings.nagios_service
+
+class ADaemon(object):
     pynag_daemon = None
 
     def __init__(self):
